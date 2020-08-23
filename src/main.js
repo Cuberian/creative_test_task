6054202776fa007a82f515eb46e254837d238f13
Igor Krupkin <48021172+cuberian@users.noreply.github.com>
2020-08-23 17:35:37 +0500

    add vue router

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import store from './store'
+import router from './routes'
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-  store
+  router,
+  store,
+
 }).$mount('#app');
